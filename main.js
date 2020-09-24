1d29b620eee6402e33994bcfa0d36d1f83a756c2
Saúl Olguin Alanis <DonSaul@users.noreply.github.com>
2020-09-24 12:48:21 -0500

    form con upload a drive

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const db = firebase.firestore();
-function myFunction() {
-   var x = document.getElementById("main");
-   if (x.style.display === "block") {
-     x.style.display = "none";
-   
-   } else {
-     x.style.display = "block";
 
-   }
- }
+const enviar = document.getElementById("main");
+
+const getexam = () => db.collection("examenes").get();
+
+
+enviar.addEventListener("submit", e =>{
+    e.preventDefault();
+    const pregunta = enviar["Pregunta"].value;
+    const recurso = enviar["ReC"].value;
+    const respc = enviar["ResC"].value;
+    const resin1 = enviar["ResINC1"].value;
+    const resin2 = enviar["ResINC2"].value;
+    const resin3 = enviar["ResINC3"].value;
+
+    const response =  db.collection("examenes").doc().set({
+      pregunta,recurso,respc,resin1,resin2,resin3
+    })
+
+
+    console.log(response)
+    console.log(pregunta,recurso,respc,resin1,resin2,resin3)
+})
+
+  window.addEventListener("DOMContentloaded", async (e)=>{
+
+         const exam = await  getexam();
+          console.log(exam)
+
+
+  })
