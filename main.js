4f7c1e855c341c82d157718ec64b2f489db4eabf
Saúl Olguin Alanis <DonSaul@users.noreply.github.com>
2020-09-24 14:33:31 -0500

    escritura de preguntas

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const db = firebase.firestore();
 
 const enviar = document.getElementById("main");
 
-const getexam = () => db.collection("examenes").get();
 
 
-enviar.addEventListener("submit", e =>{
+
+
+enviar.addEventListener("submit", async(e) =>{
     e.preventDefault();
     const pregunta = enviar["Pregunta"].value;
     const recurso = enviar["ReC"].value;
@@ -14,19 +15,40 @@ enviar.addEventListener("submit", e =>{
     const resin2 = enviar["ResINC2"].value;
     const resin3 = enviar["ResINC3"].value;
 
-    const response =  db.collection("examenes").doc().set({
+    const response =  await db.collection("examenes").doc().set({
       pregunta,recurso,respc,resin1,resin2,resin3
     })
 
 
     console.log(response)
     console.log(pregunta,recurso,respc,resin1,resin2,resin3)
+    alert("Pregunta Enviada")
 })
 
-  window.addEventListener("DOMContentloaded", async (e)=>{
 
-         const exam = await  getexam();
-          console.log(exam)
+const getexam = () => db.collection("examenes").get();
+const quizcontainer = document.getElementById("preguntased");
+
+window.addEventListener("DOMContentLoaded", async(e) => {
+  
+          const querySnapshot = await getexam();
+      
+          querySnapshot.forEach(doc => {
+         
+            console.log(doc.data())
+            const datapregunta = doc.data();
+            quizcontainer.innerHTML += `<div>
+            
+            ${datapregunta.pregunta}<br>
+           
+            <div>
+            <button class="btn btn-primary">Editar</button>
+            <button class="btn btn-secondary">Borrar</button></div>
+            </div>`
+          });
+    });
 
 
-  })
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
